ef37c93efe6175bd88dd43443a5f943831fc6937
Syotaro Yasunishi <yasu7az.tanisyo@gamil.com>
2023-10-26 13:07:10 +0900

    Change plugulam

--- test.bash
+++ test.bash
@@ -5,8 +5,14 @@ ng () {
 	echo NG at Line $1
 	res=1
 }
+
+res=0
+
+### I/O TEST ###
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 15 ]
+[ "${out}" = 14 ] || ng ${LINENO}
 
+[ "$res" = 0 ] && echo OK
+exit $res
 
