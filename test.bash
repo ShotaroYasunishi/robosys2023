1d144c2fcc8b021d8a5f74910ad3ef80a8c0ed21
Syotaro Yasunishi <yasu7az.tanisyo@gamil.com>
2023-10-26 12:59:44 +0900

    Change plugulam

--- test.bash
+++ test.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+# SPDX-FileCopyrightText: 2023 Shotaro Yasunishi
+# SPDX-License -Identifier: BSD-3-Clause
+ng () {
+	echo NG at Line $1
+	res=1
+}
 out=$(seq 5 | ./plus)
 
 [ "${out}" = 15 ]
